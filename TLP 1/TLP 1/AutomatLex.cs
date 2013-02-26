0fa014a5cc7567f5a451b7a80e93ab96315271a7
Denis <RedVarvar@yandex.ru>
2013-02-26 19:19:58 +0400

    Add project

---
+++ TLP 1/TLP 1/AutomatLex.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace TLP_1
+{    
+    class AutomatLex : ConstTables
+    {
+        string _parsedString; // обрабатываемая строка
+
+        int count_I = 0; // номер для создания кода для идентификаторов
+        int count_C = 0; // номер для создание кода для чисел
+
+        List <string> IDs_values = new List<string>(); // список кодов для идентификаторов
+        List<string> Const_values = new List<string>(); // список кодов для чисел
+
+        StreamWriter file = new StreamWriter(@"C:\book\test.txt"); // переменная для записи в файл
+        
+        // таблица для идентификаторов
+        Dictionary<string, string> IDs = new Dictionary<string, string>();
+        // таблица для чисел
+        Dictionary<string, string> numbers = new Dictionary<string, string>();
+
+        public AutomatLex(string s)
+        {
+            _parsedString = s;
+        }
+
+        ~AutomatLex()
+        {
+            file.Close();
+        }
+
+        // проверка, является ли символ буквой
+        public bool IsCharacter(char ch)
+        {
+            if (Convert.ToInt32(ch) >= Convert.ToInt32('a') && Convert.ToInt32(ch) <= Convert.ToInt32('z')
+                    || Convert.ToInt32(ch) >= Convert.ToInt32('A') && Convert.ToInt32(ch) <= Convert.ToInt32('Z')
+                    || ch == '_')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // проверка, является ли символ цифрой
+        public bool IsDigit(char ch)
+        {
+            if (ch >= '0' && ch <= '9')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // проверка, является ли символ началом операции
+        public bool IsOperation(char ch)
+        {
+            if (ch == '.' || ch == '-' || ch == '+' || ch == '~' || ch == '!' || ch == '&' || ch == '*'
+                || ch == '/' || ch == '%' || ch == '<' || ch == '>' || ch == '=' || ch == '^' || ch == '|'
+                || ch == '?' || ch == ',')
+                return true;
+            else
+                return false;
+        }
+
+        public void StartAutomat()
+        {
+            int position = 0;
+
+            if (IsCharacter(_parsedString[position]))
+            {
+                WordsFunc(ref position);
+            }
+
+            file.Close();
+ 
+        }
+
+        // обрабатывает ветку автомата, соотвествующую ситуации, когда первый символ - буква
+        public void WordsFunc(ref int position)
+        {
+            int i = position;
+            string temp_s;
+            int pos = 0;
+
+            while (true)
+            {
+                if (IsCharacter(_parsedString[i]) || IsDigit(_parsedString[i]))
+                {
+                    i++;
+                    continue;
+                }
+
+                // встречаем начало индексации
+                if (_parsedString[i] == '[')
+                {
+                    pos = i;
+                    //выделяем идентификатор и если он соотвествует служебному слову, то переходим в состояние ошибки
+                    temp_s = _parsedString.Substring(position, i - position);
+                    if (_reservedWords.ContainsKey(temp_s))
+                    {
+                        //Переход в состояние ошибки
+                    }
+
+                    // если мы нашли это идентификтор в таблицы идентификаторов, то записываем соотвествующий код в файл
+                    if (IDs.ContainsKey(temp_s))
+                    {
+                        file.Write(IDs[temp_s]);
+                        file.Write(" ");
+                    }
+                    // иначе добавляем его в таблицу идентификаторов и потом записываем в файл
+                    else
+                    {
+                        IDs_values[count_I] = "I" + Convert.ToString(count_I + 1);
+                        IDs.Add(temp_s, IDs_values[count_I]);
+                        count_I++;
+                        file.Write(IDs[temp_s]);
+                        file.Write(" ");
+                    }
+
+                    // добавляем код разделителя в файл
+                    file.Write(_separators["["]);
+                    file.Write(" ");
+                    ++i;
+                    position = i;
+                    continue;
+                }
+
+                // встречаем конец индексации
+                if (_parsedString[i] == ']')
+                {
+                    temp_s = _parsedString.Substring(pos + 1, i - pos + 1);
+
+                    // проверяем, является ли индекс массива числом
+                    if (IsDigit(temp_s[0]))
+                    {
+                        // если число уже есть в таблице, то записываем его код в файл
+                        if (numbers.ContainsKey(temp_s))
+                        {
+                            file.Write(numbers[temp_s]);
+                            file.Write(" ");
+                        }
+                        // иначе добавляем его в таблицу и записываем
+                        else
+                        {
+                            Const_values[count_C] = "C" + Convert.ToString(count_C + 1);
+                            numbers.Add(temp_s, Const_values[count_C]);
+                            count_C++;
+                            file.Write(numbers[temp_s]);
+                            file.Write(" ");
+                        }
+                    }
+                    // если это переменная, то записываем её код, иначе переходим в состояние ошибки
+                    else if (IsCharacter(temp_s[0]))
+                    {
+                        if (IDs.ContainsKey(temp_s))
+                        {
+                            file.Write(IDs[temp_s]);
+                            file.Write(" ");
+                        }
+                        else
+                        {
+                            // переходим в состояние ошибки
+                        }
+                    }
+                    else
+                    {
+                        // переходим в состояние ошибки
+                    }
+
+                    file.Write(_separators["]"]);
+                    file.Write(" ");
+                    ++i;
+                    position = i;
+                    continue;
+                }
+
+                // если встречаем пробел или конец строки, то добавляем в файл код обработанного слова
+                if (_parsedString[i] == '_' || _parsedString[i] == '\0')
+                {
+                    temp_s = _parsedString.Substring(position, position - i + 1);
+
+                    if (_reservedWords.ContainsKey(temp_s))
+                    {
+                        file.Write(_reservedWords[temp_s]);
+                        file.Write(" ");
+                    }
+                    else if (IDs.ContainsKey(temp_s))
+                    {
+                        file.Write(IDs[temp_s]);
+                        file.Write(" ");
+                    }
+                    else
+                    {
+                        IDs_values[count_I] = "I" + Convert.ToString(count_I + 1);
+                        IDs.Add(temp_s, IDs_values[count_I]);
+                        count_I++;
+                        file.Write(IDs[temp_s]);
+                        file.Write(" ");
+                    }
+
+                    if (_parsedString[i] == ' ')
+                    {
+                        ++i;
+                        position = i;
+                    }
+                    break;
+                }
+
+                // если встречаем символ операции, то вызываем функцию для обработки операций
+                if (IsOperation(_parsedString[i]))
+                {
+                    OperFunc(ref position);
+                }
+            }
+
+        }
+
+        public void OperFunc (ref int position)
+        {
+        }
+    }
+}
---
+++ TLP 1/TLP 1/ConstTables.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TLP_1
+{
+    class ConstTables
+    {
+        protected Dictionary<string, string> _reservedWords = new Dictionary<string, string>()
+        {
+                {"auto", "W1"},
+                {"break", "W2"},
+                {"case","W3"},
+                {"char","W4"},
+                {"const","W5"},
+                {"continue", "W6"},
+                {"default","W7"},
+                {"do","W8"},
+                {"double","W9"},
+                {"else","W10"},
+                {"enum", "W11"},
+                {"extern", "W12"},
+                {"float", "W13"},
+                {"for", "W14"},
+                {"goto", "W15"},
+                {"if", "W16"},
+                {"int", "W17"},
+                {"long", "W18"},
+                {"register", "W19"},
+                {"return", "W20"},
+                {"short", "W21"},
+                {"signed", "W22"},
+                {"sizeof", "W23"},
+                {"static", "W24"},
+                {"struct", "W25"},
+                {"switch", "W26"},
+                {"typedef", "W27"},
+                {"union", "W28"},
+                {"unsigned", "W29"},
+                {"void", "W30"},
+                {"volatile", "W31"},
+                {"while", "W32"}
+        };
+
+        protected Dictionary<string, string> _operations = new Dictionary<string, string>()
+        {
+                {".", "O1"},
+                {"->", "O2"},
+                {"++","O3"},
+                {"--","O4"},
+                {"~","O5"},
+                {"!", "O6"},
+                {"-","O7"},
+                {"+","O8"},
+                {"&","O9"},
+                {"*","O10"},
+                {"/", "O11"},
+                {"%", "O12"},
+                {"<<", "O13"},
+                {">>", "O14"},
+                {"<", "O15"},
+                {">", "O16"},
+                {"<=", "O17"},
+                {">=", "O18"},
+                {"==", "O19"},
+                {"!=", "O20"},
+                {"^", "O21"},
+                {"|", "O22"},
+                {"&&", "O23"},
+                {"||", "O24"},
+                {"? :", "O25"},
+                {"=", "O26"},
+                {"+=", "O27"},
+                {"-=", "O28"},
+                {"*=", "O29"},
+                {"/=", "O30"},
+                {"%=", "O31"},
+                {"<<=", "O32"},
+                {">>=", "O33"},
+                {"&=", "O34"},
+                {"^=", "O35"},
+                {"|=", "O36"},
+                {",", "O37"}
+        };
+
+        protected Dictionary<string, string> _separators = new Dictionary<string, string>()
+        {
+            {":","S1"},
+            {";","S2"},
+            {"(","S3"},
+            {")", "S4"},
+            {"{", "S5"},
+            {"}", "S6"},
+            {"/", "S7"},
+            {"space", "S8"},
+            {"/0", "S9"},
+            {"[", "S10"},
+            {"]", "S11"}
+        };
+    }
+}

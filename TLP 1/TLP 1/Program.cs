49c86049fc52fccef04056e7350eaf6d536500c4
Denis <RedVarvar@yandex.ru>
2013-04-17 18:34:29 +0400

    Добавил перевод в ОПЗ. Немного подправил лексический анализатор

--- TLP 1/TLP 1/Program.cs
+++ TLP 1/TLP 1/Program.cs
@@ -2,16 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace TLP_1
 {
     class Program
-    {
+    {      
         static void Main(string[] args)
         {
-            string temp = "a[2] vd: d; 1;";
-            AutomatLex a = new AutomatLex(temp);
-            a.StartAutomat();
+            StreamReader fileInput = new StreamReader(@"input.txt");
+            string temp;
+            int u = 0;
+            Automat a = new Automat();
+
+            while (true)
+            {
+                temp = (fileInput.ReadLine());
+
+                if (temp != null)
+                {
+                    a.StartAutomat(temp);
+                }
+                else
+                {
+                    a.TableWrite();
+                    a.CloseFile();
+                    break;
+                }
+            }
+
+            StreamReader fileOutput = new StreamReader(@"output.txt");
+
+            while (true)
+            {
+                string s = fileOutput.ReadLine();
+
+                if (s != null)
+                {
+                    temp = temp + " " + s;
+                }
+                else
+                {
+                    fileOutput.Close();
+                    break;
+                }
+            }
+
+            temp = temp.Substring(1);
+
+            RPN r = new RPN(temp);
+
+            r.StartRPN(ref u);
+            r.CloseFile();
         }
     }
 }
